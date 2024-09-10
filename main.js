02de561edd188b71cf3b69cb213bd3b74b0fedca
vortix2950 <vortix2950@ksu.edu>
2024-09-10 11:08:17 +0300

    solved

---
+++ main.js
@@ -0,0 +1,40 @@
+const User ={
+    name:"John",
+    email: "johnn@hotmail.com",
+    id:1,
+    Available : true,
+    isAvilable : function (){this.Available? console.log("user is available"):console.log("user is not available")},
+    adress:{
+        objectCity:{
+            name :"london",
+            population: 2300000
+        },
+        street: "some street",
+        zipCode: 15354
+
+    },
+    skills:["cooking","yelling", "mountain falling"]
+
+}
+
+console.log(User)
+console.log(User["name"])
+User.id =3
+console.log(User)
+
+delete User.email
+
+console.log(User)
+
+User.isAvilable()
+
+User.Available=false
+
+console.log(User)
+
+User.isAvilable()
+
+
+console.log(User.adress.objectCity.name)
+
+console.log(User.skills.join(','))
